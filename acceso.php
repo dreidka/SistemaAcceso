7a2d95583851d3befc68dcc559528d7bbba10107
Felidka <80848004+Felidka@users.noreply.github.com>
2023-11-18 17:32:13 -0500

    Version 2.0

--- acceso.php
+++ acceso.php
@@ -3,24 +3,35 @@
             $username = "root";
             $password = "";
             $dbname = "qr_basedatos";
-
+            $time = date('H:m:s');
+            $date = date('Y-m-d');
             $con = new mysqli($server,$username,$password,$dbname);
-        
             if($con->connect_error){
                 die("Connection failed" .$con->connect_error);
             }
             if(isset($_POST['submit'])){
                 $txtID = $_POST['txtID'];
+                $txtName = $_POST['txtNombre'];
                 $llegada = $_POST['opcLlegada'];
                 $placa = $_POST['txtPlaca'];
-                $sql = "INSERT INTO acceso (IdUsuario,HoraEntrada,FormaLlegada,Placa) values('$txtID',NOW(),'$llegada','$placa')";
-                $conn = $con->query($sql);
-                if($conn===TRUE){
-                    echo "Usuario registrado";
-                    header("location: insert1.php");
+                $estado = $_POST['txtEstado'];
+                $sql = "SELECT * FROM acceso WHERE IdUsuario = '$txtID' AND Estado='1' ";
+                $query = $con->query($sql);
+                if($query->num_rows>0){
+                    $sql = "UPDATE acceso SET HoraSalida=NOW(), Estado = '0' WHERE IdUsuario = '$txtID'";
+                    $query = $con->query($sql);
+                    header("location: index.php");
                 }else{
-                    echo "Deseas ingresar usuario nuevo?";
+                    $sql = "INSERT INTO acceso (IdUsuario,HoraEntrada,Nombre,FormaLlegada,Placa,Estado) values('$txtID',NOW(),'$txtName','$llegada','$placa','$estado')";
+                    $conn = $con->query($sql);
+                    if($conn===TRUE){
+                        echo "Usuario registrado";
+                        header("location: index.php");
+                    }else{
+                        echo "Deseas ingresar usuario nuevo?";
+                    }
                 }
+               
             }
             $con->close();
 ?>
--- index.php
+++ index.php
@@ -1,12 +1,58 @@
 <html>
     <head>
 
-<script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
+    <script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
     </head>
     <body>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <td>ID</td>
+                        <td>USUARIO</td>
+                        <td>NOMBRE</td>
+                        <td>HORA ENTRADA</td>
+                        <td>HORA SALIDA</td>
+                        <td>MEDIO LLEGADA</td>
+                        <td>PLACA</td>
+                        <td>ESTADO</td>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $server = "localhost";
+                    $username = "root";
+                    $password = "";
+                    $dbname = "qr_basedatos";
+          
+                    $con2 = new mysqli($server,$username,$password,$dbname);
+                    if($con2->connect_error){
+                        die("Connection failed" .$con2->connect_error);
+                    }
+                    $sql2 = "SELECT IdIngreso, IdUsuario, Nombre, HoraEntrada, HoraSalida, FormaLlegada, Placa, Estado from acceso";
+                    $query2 = $con2->query($sql2);
+                    while($row= $query2->fetch_assoc()){
+                    ?>
+                    <tr>
+                        <td><?php echo $row['IdIngreso']?></td>
+                        <td><?php echo $row['IdUsuario']?></td>
+                        <td><?php echo $row['Nombre']?></td>
+                        <td><?php echo $row['HoraEntrada']?></td>
+                        <td><?php echo $row['HoraSalida']?></td>
+                        <td><?php echo $row['FormaLlegada']?></td>
+                        <td><?php echo $row['Placa']?></td>
+                        <td><?php echo $row['Estado']?></td>
+                        
+                    </tr>
+                <?php
+                    }
+                ?>
+                </tbody>
+            </table>
+   
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
